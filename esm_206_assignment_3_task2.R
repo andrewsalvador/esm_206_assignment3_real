fae70d0b2359f92d61a648f3b76e2b7189fe55e9
andrewsalvador <andrewsalvador@ucsb.edu>
2019-10-22 13:29:23 -0700

    task 2 updates, and good graph green rect changed to brown

---
+++ esm_206_assignment_3_task2.R
@@ -0,0 +1,73 @@
+# Andrew Salvador
+# ESM 206, Assignment 3, Task 2
+# 21 October 2019
+
+
+library(tidyverse)
+library(hexbin)
+library(tidyr)
+
+space_launches <- read_csv("space_launches.csv")
+
+# Mess around wrangling
+space_data <- space_launches %>% 
+  select(launch_date:category, -state_code, -variant) %>% 
+  filter(launch_year >= 1994) %>% 
+  arrange(launch_year) %>% 
+  filter(agency == "US")
+
+
+
+# Random really bad graph
+ggplot(space_launches, aes(x = launch_year,
+                       y = agency)) +
+  geom_hex(bins = 5000) +
+  scale_fill_continuous(type = "viridis") +
+  geom_point(aes(x = launch_year,
+                 y = agency),
+                 size = .1) +
+  geom_hline(aes(yintercept = 16,
+                 color = "pink"))
+
+#Other really bad graph (this one is better to turn in)
+ggplot(space_launches, aes(x = launch_year,
+                           y = agency)) +
+  geom_count(aes(color = launch_year)) +
+  theme(legend.position = "left") +
+  annotate(geom = "rect",
+           xmax = 1992,
+           xmin = 1958,
+           ymax = 42,
+           ymin = 39,
+           fill = "blue",
+           alpha = .2) +
+  annotate(geom = "text",
+           x = 1973,
+           y = 40.5,
+           label = "a lot",
+           color = "blue4") +
+  geom_vline(aes(xintercept = 1991),
+               color = "pink1") +
+  scale_y_continuous(limits(0,0))
+  coord_flip()
+
+
+
+ 
+ 
+  
+
+
+
+
+
+ 
+   
+
+  
+  
+
+
+
+
+           
